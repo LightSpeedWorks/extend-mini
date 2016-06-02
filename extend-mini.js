081fcacce20349a35b982a2e86e4cdf572dcaa11
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-06-03 08:49:41 +0900

    revise, add comment

--- extend-mini.js
+++ extend-mini.js
@@ -1,15 +1,7 @@
 this['extend-mini'] = function () {
   'use strict';
 
-  // merge-light
-  function merge(dst, src) {
-    for (var i = 1; src = arguments[i], i < arguments.length; ++i)
-      for (var p in src)
-        if (src.hasOwnProperty(p) && !dst.hasOwnProperty(p) &&
-            dst[p] !== src[p]) dst[p] = src[p];
-    return dst;
-  }
-
+  // extend
   function extend(proto, statics) {
     'use strict';
     proto = proto || {};
@@ -31,10 +23,20 @@ this['extend-mini'] = function () {
       super_ ? {super_: super_} : undefined, super_, {extend: extend, create: create});
   }
 
+  // create
   function create() {
     function $ctor() {}
     $ctor.prototype = this.prototype;
     return this.apply(new $ctor(), arguments);
+  }
+
+  // merge-light
+  function merge(dst, src) {
+    for (var i = 1; src = arguments[i], i < arguments.length; ++i)
+      for (var p in src)
+        if (src.hasOwnProperty(p) && !dst.hasOwnProperty(p) &&
+            dst[p] !== src[p]) dst[p] = src[p];
+    return dst;
   }
 
   if (typeof module === 'object' && module && module.exports)
